e273103e5f2f613429204dc5d81009b5b1e19220
moshearz <moshearzuan1@gmail.com>
2024-12-12 19:40:39 +0200

    Deleted the workshop files.Finished

--- moshe.py
+++ moshe.py
@@ -1,2 +1,2 @@
-print("Hi my name is Moshe Arzuan, and we missed a flight because we fall asleep in the airport")
+
 print("Hello world")
--- test.py
+++
@@ -1,5 +0,0 @@
-def print_funny_story():
-    print("I drove yo Beer Sheba and bought a sword for 300NIS")
-
-
-print_funny_story()
