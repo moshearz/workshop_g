7c098b181f40bf8179548c94439996b5fbc74c03
moshearz <moshearzuan1@gmail.com>
2024-12-12 12:29:39 +0200

    added a new func: print_childhoodStory

--- moshe.py
+++ moshe.py
@@ -1,1 +1,8 @@
 print("Hi my name is Moshe Arzuan, and we missef a flight because we fall asleep in the airport")
+
+def print_childhoodStory():
+    print("Enter a childhood story: ")
+    childhoodStory = input()
+    print("Let me tell you a childhood story: " + childhoodStory)
+
+print_childhoodStory()
