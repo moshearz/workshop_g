ae5a292c92440beb14c89fec8576823361f103ef
moshearz <moshearzuan1@gmail.com>
2024-12-12 12:39:15 +0200

    created commit

--- moshe.py
+++ moshe.py
@@ -1,8 +1,8 @@
 print("Hi my name is Moshe Arzuan, and we missef a flight because we fall asleep in the airport")
 
-def print_childhoodStory():
+def conflict():
     print("Enter a childhood story: ")
     childhoodStory = input()
     print("Let me tell you a childhood story: " + childhoodStory)
 
-print_childhoodStory()
+#print_childhoodStory()
